076c16d6704c0fed69c3309033a98b0547afd3af
name <junseoks9306@gmail.com>
2022-03-03 16:58:23 +0900

    refactor : 공백 제거

--- app/src/main/java/View/MainActivity.kt
+++ app/src/main/java/View/MainActivity.kt
@@ -24,16 +24,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val layout = findViewById<FrameLayout>(R.id.frame_layout)
-        val drawObjects = DrawObjects(layout.context)
-        layout.addView(drawObjects)
+
+        val button2 = findViewById<Button>(R.id.make_square_button)
+
     }
 }
 
 
-class DrawObjects(context: Context?) : View(context) {
+class DrawObjects(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
     private val domain = Domain()
     private var rectangles: MutableList<Rectangles> = domain.getRandomRect()
+
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
@@ -44,8 +45,10 @@ class DrawObjects(context: Context?) : View(context) {
             ViewGroup.LayoutParams.WRAP_CONTENT
         )
         button.layoutParams = setLocation
-        val layout = (this.parent) as FrameLayout
+        val layout = (this.parent) as ConstraintLayout
         layout.addView(button)
+
+
         setBackgroundColor(Color.DKGRAY)
 
         val loopCount = rectangles.count()
@@ -58,7 +61,28 @@ class DrawObjects(context: Context?) : View(context) {
         }
     }
 
-    override fun onTouchEvent(event: MotionEvent?): Boolean {
-        return super.onTouchEvent(event)
-    }
+//    override fun onTouchEvent(event: MotionEvent?): Boolean {
+//
+//        val x = event?.x
+//        val y = event?.y
+//
+//        when(event?.action) {
+//            MotionEvent.ACTION_DOWN ->
+//
+//
+//            MotionEvent.ACTION_MOVE ->
+//
+//
+//            MotionEvent.ACTION_UP ->
+//
+//            else ->
+//        }
+//
+//
+//
+//
+//        return super.onTouchEvent(event)
+//
+//
+//    }
 }
