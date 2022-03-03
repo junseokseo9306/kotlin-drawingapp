6d9598a71012e8b70da4c55b9b6f16496d05fb62
name <junseoks9306@gmail.com>
2022-03-03 10:06:48 +0900

    refactor : 레이아웃 정리 및 사각형 랜덤 위치 구현

--- app/src/main/java/Data/RectFactory.kt
+++ app/src/main/java/Data/RectFactory.kt
@@ -6,14 +6,16 @@ import android.graphics.RectF
 
 class RectFactory : RandomRectGenerator {
     override fun makeRandomRect(): Rectangles {
+        val squareWidth = 150.toFloat()
+        val squareHeight = 150.toFloat()
         val charA = (97..122).random().toChar()
         val charB = (97..122).random().toChar()
         val charC = (97..122).random().toChar()
         val randomID = "$charA$charB$charC"
-        val x = (10..600).random().toFloat()
-        val y = (90..500).random().toFloat()
-        val w = 150.toFloat()
-        val h = 120.toFloat()
+        val x = (0..1000).random().toFloat()
+        val y = (0..1000).random().toFloat()
+        val w = x + squareWidth
+        val h = y + squareHeight
         val r = (0..255).random()
         val g = (0..255).random()
         val b = (0..255).random()
--- app/src/main/java/View/MainActivity.kt
+++ app/src/main/java/View/MainActivity.kt
@@ -8,10 +8,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Layout
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.FrameLayout
 import androidx.appcompat.widget.ContentFrameLayout
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import com.example.app.R
 import com.google.android.material.snackbar.Snackbar
 import View.MainActivity as MainActivity1
@@ -20,10 +23,10 @@ import android.view.View as View
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
-        val drawObjects = DrawObjects(this)
-        setContentView(drawObjects)
-
+        setContentView(R.layout.activity_main)
+        val layout = findViewById<FrameLayout>(R.id.frame_layout)
+        val drawObjects = DrawObjects(layout.context)
+        layout.addView(drawObjects)
     }
 }
 
@@ -31,9 +34,9 @@ class MainActivity : AppCompatActivity() {
 class DrawObjects(context: Context?) : View(context) {
     private val domain = Domain()
     private var rectangles: MutableList<Rectangles> = domain.getRandomRect()
-
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+
         val button = Button(context)
         button.text = "사각형"
         val setLocation = ConstraintLayout.LayoutParams(
@@ -41,18 +44,21 @@ class DrawObjects(context: Context?) : View(context) {
             ViewGroup.LayoutParams.WRAP_CONTENT
         )
         button.layoutParams = setLocation
-        val layout = (this.parent) as ContentFrameLayout
+        val layout = (this.parent) as FrameLayout
         layout.addView(button)
-        setBackgroundColor(Color.WHITE)
+        setBackgroundColor(Color.DKGRAY)
 
         val loopCount = rectangles.count()
         for(i in 0 until loopCount) {
             canvas?.drawRect(rectangles[i].rectangles, rectangles[i].paint)
         }
-
         button.setOnClickListener {
             rectangles = domain.getRandomRect()
             invalidate()
         }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        return super.onTouchEvent(event)
     }
 }
